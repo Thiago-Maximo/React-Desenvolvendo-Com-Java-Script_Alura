8635c060d110cc02a409e185e8498882bdcdbcd4
Thiago-Maximo <142333508+Thiago-Maximo@users.noreply.github.com>
2025-09-17 11:51:52 -0300

    Primeiro Componente React
    Criado o primeiro componente utilizando react, css e js

---
+++ organo/src/Componentes/banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner(){
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner Principal da Pagina"></img>
+        </header>
+    )
+}
+
+export default Banner
