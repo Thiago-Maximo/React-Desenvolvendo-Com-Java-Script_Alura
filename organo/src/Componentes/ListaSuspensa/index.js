2f0463b057ba33578df080513e3ae48caca8a43e
SIND-EAA <142333508+Thiago-Maximo@users.noreply.github.com>
2025-09-18 11:28:35 -0300

    Submissão do Form
    O form não esta enviando as informações ainda, mas estamos conseguindo capturar as informações dos campos

--- organo/src/Componentes/ListaSuspensa/index.js
+++ organo/src/Componentes/ListaSuspensa/index.js
@@ -1,15 +1,19 @@
 import './lista.css'
 
 const ListaSuspensa = (props) => {
-    console.log(props.itens)
-    return (
-        <div className='ListaSuspensa'>
-            <label>{props.label}</label>
-            <select>
-                {props.itens.map(item => <option key={item}>{item}</option>)}
-            </select>
-        </div>
-    )
+  return (
+    <div className='ListaSuspensa'>
+      <label>{props.label}</label>
+      <select 
+        onChange={evento => props.aoAlterado(evento.target.value)} 
+        required={props.required} 
+        value={props.value}
+      >
+        {props.itens.map(item => <option key={item}>{item}</option>)}
+      </select>
+    </div>
+  )
 }
+
 
 export default ListaSuspensa
--- organo/src/Componentes/formulario/index.js
+++ organo/src/Componentes/formulario/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import BotaoEnviar from '../BotaoEnviar_Formulario';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
@@ -6,6 +7,7 @@ import './formulario.css';
 const Formulario = () => {
 
     const times = [
+        '',
         'Programação',
         'Front-End',
         'Data-Science',
@@ -14,19 +16,48 @@ const Formulario = () => {
         'UX e Desgin'
     ]
 
-    const aoSalvar= (evento) =>{
+    const aoSalvar = (evento) => {
         evento.preventDefault();
-        console.log('Form Foi Enviado')
+        console.log('Form Foi Enviado', nome, cargo, imagem, time )
     }
-    
+
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os Dados Para Criar o Card Do Colaborador</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite Seu Nome" />
-                <CampoTexto label="Cargo" placeholder="Digite Seu Cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o Endereço da Imagem" />
-                <ListaSuspensa label='Time' itens={times}></ListaSuspensa>
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite Seu Nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true} label="Cargo"
+                    placeholder="Digite Seu Cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o Endereço da Imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    required={true}
+                    label='Time'
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                ></ListaSuspensa>
                 <BotaoEnviar texto="Criar Card"></BotaoEnviar>
             </form>
         </section>
